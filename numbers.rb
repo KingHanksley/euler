ba861fcace9766bc5a6e14d7913ad423cbb2724f
David Iscoe <discoe@gmail.com>
2016-03-18 01:47:28 -0400

    three more challenges

--- numbers.rb
+++ numbers.rb
@@ -82,7 +82,63 @@ def breaktoprime(int)
 	return array
 end
 
+def is_palin(int)
+	int.to_s.reverse == int.to_s
+end
 
-breaktoprime(600851475143)
+
+def iterate_products
+#doesn't work as advertised
+	a = 999
+	
+	while(a >= 100) do
+		b = 999
+		while (a<=b) do
+			#p a
+			#p b
+			#p a*b
+			p "PALINDROME!#{a} x #{b} = #{a*b}" if is_palin(a*b)
+			b-= 1
+		end
+		a -= 1
+	end
+end
+
+def evenlydivisible 
+	primes = 2*3*5*7*11*13*17*19
+	max = 2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20
+	a = primes
+	while (a<max)
+		clean = true
+		for i in 1..20
+			clean = false if a%i > 0
+		end
+		p a 
+		p "#{a} is clean!" if clean
+		return a if clean 
+		a+= primes
+	end
+end
+
+def sumsquarediff(int)
+sumsq = 0
+
+for i in 0..int
+	sumsq+= i * i
+end
+
+p sumsq
+a=*(1..int) 
+p a
+sqsum = sum(a)**2
+p sqsum
+p "Square of sum (#{sqsum}) minus sum of squares (#{sumsq}) equals #{sqsum-sumsq}"
+end
+
+sumsquarediff(100)
+#evenlydivisible
+
+
+#breaktoprime(1342124124)
 #p primesupto(356)
 #p findfactors(600851475143)
